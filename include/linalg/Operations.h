0c91017040f3bb69ccce4c382a0f77a6f9c6187b
SeaWal <sean.walshe@tutanota.com>
2024-06-02 17:18:59 +0100

    implemented matrix subtract and flatten operations

--- include/linalg/Operations.h
+++ include/linalg/Operations.h
@@ -5,5 +5,26 @@
 
 namespace linalg {
     Matrix mat_add(const linalg::Matrix& lhs, const linalg::Matrix& rhs);
+    Matrix mat_subtract(const linalg::Matrix& lhs, const linalg::Matrix& rhs);
+    Matrix mat_flatten(const linalg::Matrix& mat, bool as_rowvec = true);
+    // Matrix mat_transpose(const linalg::Matrix& mat);
+    // Matrix mat_multiply(const linalg::Matrix& lhs, const linalg::Matrix& rhs);
+    // Matrix scalar_multiply();
+    // Matrix scalar_add();
+    // Matrix hadamard_product();
+
+    // double trace();
+    // double det();
+
+    // double dot(std::vector<double> vec1, std::vector<double> vec2);
+
+    // Matrix-vector multiplication
+    // Matrix inverse/pinverse
+    // Eigenvalue/Eigenvectors
+    // Covariance
+    // SVD
+    // PCA
+
+
 }
 
--- src/linalg/Operations.cpp
+++ src/linalg/Operations.cpp
@@ -16,3 +16,33 @@ linalg::Matrix linalg::mat_add(const linalg::Matrix& lhs, const linalg::Matrix& 
 
     return result;
 }
+
+
+linalg::Matrix linalg::mat_subtract(const linalg::Matrix& lhs, const linalg::Matrix& rhs)
+{
+    if(lhs.rows() != rhs.rows() && lhs.cols() != rhs.cols()) {
+        throw std::invalid_argument("Matrix dimensions must match.");
+    }
+
+    linalg::Matrix result(lhs.rows(), lhs.cols());
+    for(std::size_t i = 0; i < lhs.rows(); i++) {
+        for(std::size_t j = 0; j < lhs.cols(); j++) {
+            result(i, j) = lhs(i, j) - rhs(i, j);
+        }
+    }
+
+    return result;
+}
+
+// TODO: use Matrix copy constructor instead
+linalg::Matrix linalg::mat_flatten(const linalg::Matrix& mat, bool as_rowvec)
+{
+    linalg::Matrix flattened(1, mat.rows() * mat.cols());
+    for(std::size_t i = 0; i < mat.rows(); i++) {
+        for(std::size_t j = 0; j < mat.cols(); j++) {
+            flattened(0, i*mat.cols() + j) = mat(i, j);
+        }
+    }
+
+    return flattened;
+}
