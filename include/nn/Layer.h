45930d3e9615fa4d0d88d33e76819eb1bf141826
SeaWal <sean.walshe@tutanota.com>
2024-06-29 19:30:11 +0100

    create nn folder for neural network development. Layer class is abstract base class of all NN layers

---
+++ include/nn/Layer.h
@@ -0,0 +1,22 @@
+#include <optional>
+#include <string>
+#include <vector>
+
+#include "linalg/Matrix.h"
+
+class Layer
+{
+public:
+    Layer() = default;
+    Layer(std::optional<std::string> name = std::nullopt) : m_Name(name) {}
+    virtual ~Layer() = default;
+
+    virtual linalg::Matrix forward(const linalg::Matrix& input) = 0;
+    virtual linalg::Matrix backward(const linalg::Matrix& output_grad) = 0;
+    virtual void update_parameters(double learning_rate) = 0;
+
+    inline std::optional<std::string> name() { return m_Name; }
+
+protected:
+    std::optional<std::string> m_Name;
+};
