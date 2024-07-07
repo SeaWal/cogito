7f02938bd812b655c33e42dcb98d666c2831457b
SeaWal <sean.walshe@tutanota.com>
2024-07-07 18:26:33 +0100

    NeuralNetwork class

---
+++ include/nn/NeuralNetwork.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+
+#include "Layer.h"
+#include "linalg/Matrix.h"
+
+class NeuralNetwork
+{
+public:
+    NeuralNetwork() = default;
+    void add_layer(std::unique_ptr<Layer> layer);
+    linalg::Matrix forward(const linalg::Matrix &input);
+    void backward(const linalg::Matrix &loss_grad);
+    void update_parameters(const double learning_rate);
+    void train(const linalg::Matrix &inputs, const linalg::Matrix &targets,
+               const std::size_t epochs, const double learning_rate);
+
+private:
+    std::vector<std::unique_ptr<Layer>> m_Layers;
+};
