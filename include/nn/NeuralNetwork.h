e9b716b233eb06a8be511e5dedf3a430df519b28
SeaWal <sean.walshe@tutanota.com>
2024-07-11 16:13:12 +0100

    add NeuralNetwork class

--- include/nn/NeuralNetwork.h
+++ include/nn/NeuralNetwork.h
@@ -14,7 +14,7 @@ public:
     linalg::Matrix forward(const linalg::Matrix &input);
     void backward(const linalg::Matrix &loss_grad);
     void update_parameters(const double learning_rate);
-    void train(const linalg::Matrix &inputs, const linalg::Matrix &targets,
+    void train(const linalg::Matrix &input, const linalg::Matrix &target,
                const std::size_t epochs, const double learning_rate);
 
 private:
