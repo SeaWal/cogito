febe274f346c19375b2e9e17f1fba1843537f750
SeaWal <sean.walshe@tutanota.com>
2024-08-01 17:02:28 +0100

    Fixed init of weights, calculating forward and broadcast biases to allow addition

--- src/nn/Dense.cpp
+++ src/nn/Dense.cpp
@@ -1,22 +1,38 @@
+#include <iostream>
+
 #include "nn/Dense.h"
 #include "linalg/Matrix.h"
 #include "linalg/Operations.h"
 
 using linalg::Matrix;
 
+namespace {
+
+    Matrix broadcast_biases(const Matrix& biases, std::size_t num_samples)
+    {
+        Matrix broadcasted_biases(biases.rows(), num_samples);
+        for (std::size_t i = 0; i < num_samples; ++i) {
+            for (std::size_t j = 0; j < biases.rows(); ++j) {
+                broadcasted_biases(j, i) = biases(j, 0);
+            }
+        }
+        return broadcasted_biases;
+    }
+}
+
 Dense::Dense(std::size_t input_dim, std::size_t output_dim, std::optional<std::string> name = std::nullopt)
     : Layer(name)
 {
     m_InputDim = input_dim;
     m_OutputDim = output_dim;
-    m_Weights = Matrix::random(input_dim, output_dim);
+    m_Weights = Matrix::random(output_dim, input_dim);
     m_Biases = Matrix::random(output_dim, 1);
 }
 
 Matrix Dense::forward(const Matrix &input)
 {
     m_Input = input;
-    return (m_Weights * input) + m_Biases;
+    return (m_Weights * linalg::mat_transpose(input)) + broadcast_biases(m_Biases, input.rows());
 }
 
 Matrix Dense::backward(const Matrix &output_grad)
